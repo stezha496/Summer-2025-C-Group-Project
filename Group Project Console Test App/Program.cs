cee66f1fcc8ea0607237154efccc176629e5fefb
Steven Zhao <stezha496@gmail.com>
2025-07-27 14:34:52 -0400

    Add project files.

---
+++ Group Project Console Test App/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_Project_Console_Test_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
